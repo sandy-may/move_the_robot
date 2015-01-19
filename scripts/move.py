2d6282a83b6e7c4665c51be0b2d676a5ff628692
Sandy May <sandymay.93@gmail.com>
2015-01-19 02:56:29 +0000

    Update to publish to /map

--- scripts/move.py
+++ scripts/move.py
@@ -2,21 +2,26 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 
-def talker():
-    pub = rospy.Publisher('movePose', PoseStamped, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+def move():
+    pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+    rospy.init_node('move', anonymous=True)
     r = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         movePose = PoseStamped()
         movePose.header.frame_id = '/map'
-        movePose.pose.position.x = 1.0
-        movePose.pose.position.y = 1.0
+        movePose.pose.position.x = 0.0
+        movePose.pose.position.y = 0.0
+        movePose.pose.position.z = 0.0
+        movePose.pose.orientation.x = 0.0
+        movePose.pose.orientation.y = 0.0
+        movePose.pose.orientation.z = 0.0
         movePose.pose.orientation.w = 1.0
+        
         rospy.loginfo(movePose)
         pub.publish(movePose)
         r.sleep()
 
 if __name__ == '__main__':
     try:
-        talker()
+        move()
     except rospy.ROSInterruptException: pass
