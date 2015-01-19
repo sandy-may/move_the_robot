728cae09d02db383776c979337e69d642e803327
Sandy May <sandymay.93@gmail.com>
2015-01-19 23:27:17 +0000

    Add Corrifor behaviour

--- scripts/move.py
+++ scripts/move.py
@@ -4,15 +4,16 @@ from geometry_msgs.msg import PoseStamped
 from move_base_msgs.msg import MoveBaseActionResult
 
 flag = False
-
+#move function accepts x and y coordinate
 def move(x, y):
     while not flag:
+        #create a move base publisher
         pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
     
         r = rospy.Rate(10) #10hz
-    
+        #create a PoseStamped object and pass values
         movePose = PoseStamped()
-        movePose.header.frame_id = '/map'
+        movePose.header.frame_id = '/map' #set map as frame id
         movePose.pose.position.x = x
         movePose.pose.position.y = y
         movePose.pose.position.z = 0.0
@@ -21,11 +22,11 @@ def move(x, y):
         movePose.pose.orientation.z = 0.0
         movePose.pose.orientation.w = 1.0
     
-        rospy.loginfo(movePose)
-        pub.publish(movePose)
+        rospy.loginfo(movePose) #pass info to terminal
+        pub.publish(movePose) #pass info to any subscribers
         r.sleep()
         
-        listener()
+        listener()#call listener
     
 #listener to subscribe to move_baser/result
 def listener():
@@ -37,19 +38,32 @@ def callback(msg):
         global flag 
         flag = True
     
-#moving the robot function
-def link():
+#main function function
+def corridor():
    
     rospy.init_node('link', anonymous=True)
    
     while not rospy.is_shutdown():
        
-        move(0, 0)
+        move(-2.5, 0.0)
 
+        global flag
+        flag = False
+        #move 1.5 meteres forward
+        move(-1.0, 0.0)
+        
+        flag = False
+        #move 1.5 meteres forward and 2 meteres left diagonally
+        move(0.5, 2.0)
+        
+        flag = False
+        #move 1.5 meteres forward and 2 meteres right diagonally
+        move(2.0, 0.0)
+        
         if flag:
             break
 
 if __name__ == '__main__':
     try:
-        link()
+        corridor()
     except rospy.ROSInterruptException: pass
